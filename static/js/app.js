9bb6d455c5775542daadc2d3c6c7bcd9787f5e5d
changrita1114 <hardheard101@gmail.com>
2020-12-03 17:06:06 -0800

    updates

--- static/js/app.js
+++ static/js/app.js
@@ -4,58 +4,54 @@ d3.json("data/samples.json").then((importedData) => {
     // console.log(importedData);
     let data = importedData;
     let sortedBySampleValues = data.samples.sort((a, b) => b.sample_values - a.sample_values);
-    console.log(sortedBySampleValues)
-    // Slice the first indivisual for plotting
-    slicedData = sortedBySampleValues.slice(0, 1);
-    console.log(slicedData)
-    // Reverse the array to accommodate Plotly's defaults
-    reversedData = slicedData.reverse();
-    console.log(reversedData);
-  
-    let x = reversedData.map(object => object.sample_values);
-    console.log(x);
-    let x_sliced = x.slice(0, 10);
-     console.log(x_sliced);
-    // let x=[];
-    // let y=[];
-    
-    // let test_1 = reversedData.map(recipe => recipe.x);
-    // let test_2 = reversedData.map(recipe => recipe.y);
-    // console.log(test_1)
-    // console.log(test_2)
+    // console.log(sortedBySampleValues)
 
+    // Slice the first indivisual for plotting
+    let slicedSamples = sortedBySampleValues[0].sample_values.slice(0, 10);
+    // console.log(slicedSamples)
+    let slicedID = sortedBySampleValues[0].otu_ids.slice(0, 10);
+    // console.log(slicedID)
+    let slicedLabel = sortedBySampleValues[0].otu_labels.slice(0, 10);
+    // console.log(slicedLabel)
+
+    d3.select("#selDataset")
+        .selectAll("option")
+        .data(data.names)
+        .enter().append('option').text(id => id)
+
+    d3.select("#sample-metadata").data(data.metadata)
 
     // Trace1 for the Greek Data
     let trace1 = {
-        x: reversedData.map(object => object.sample_values[0]),
-        y: reversedData.map(object => object.otu_ids[0]),
-        text: reversedData.map(object => object.otu_labels[0]),
+        x: slicedSamples.reverse(),
+        y: slicedID.map(OTU => `OTU ${OTU}`).reverse(),
+        text: slicedLabel,
         name: "OTU",
         type: "bar",
         orientation: "h"
     };
+    let trace2 = {
+        x: slicedID,
+        y: slicedSamples,
+        text: slicedLabel,
+        mode: 'markers',
+        // marker: {
+        //     size: [slicedSamples.map(item => item)]
+        // }
+    };
+     
+    console.log(slicedSamples.map(item => item))
 
-
-    // //data
-    // let chartData = [trace1, trace2];
+    // data
     let chartData = [trace1];
-    // Apply the group bar mode to the layout
-    // let layout = {
-    //     title: "OTU results",
-    //     margin: {
-    //         l: 100,
-    //         r: 100,
-    //         t: 100,
-    //         b: 100
-    //     }
-    // };
+    let chartData_2 = [trace2];
 
     // Render the plot to the div tag with id "plot"
     Plotly.newPlot("bar", chartData);
-    // Plotly.newPlot("bubble", chartData);
-    // Plotly.newPlot("gauge", chartData, layout);
-    
-    // Save this info for later part
-    console.log(data.names)
+    Plotly.newPlot("bubble", chartData_2);
+    // Plotly.newPlot("gauge", chartData_3, layout);
 });
 
+function optionChanged(val) {
+    console.log(val);
+}
